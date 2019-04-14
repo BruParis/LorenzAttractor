2070f04c1863de42268c3b3fe68f8b9f4c9689c6
BruParis <parisbruno85@gmail.com>
2019-04-14 18:06:43 +0200

    initialized lorenz attractor with viz

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <opencv2/viz/vizcore.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+void updatePoint(cv::Point3f& point, float time_delta, float sigma, float rho,
+    float beta)
+{
+    float dx = time_delta * sigma * (point.y - point.x);
+    float dy = time_delta * (rho * point.x - point.y - point.x * point.z);
+    float dz = time_delta * (point.x * point.y - beta * point.z);
+
+    cv::Point3f diff(dx, dy, dz);
+    point += diff;
+}
+
+int main()
+{
+    /// Create a window
+    viz::Viz3d myWindow("Coordinate Frame");
+
+    /// Add coordinate axes
+    myWindow.showWidget("Coordinate Widget", viz::WCoordinateSystem());
+
+    std::vector<cv::Point3f> points;
+    std::vector<cv::Point3f> colors;
+
+    /// Rodrigues vector
+    cv::Point3f point(1.0, 3.0, 1.0);
+    float sigma = 10., rho = 28., beta = 8. / 3.;
+    float time = 0.0, time_delta = 0.01;
+
+    while(!myWindow.wasStopped())
+    {
+        updatePoint(point, time_delta, sigma, rho, beta);
+        points.push_back(point);
+
+        time += time_delta;
+
+        viz::WCloud cloud_widget = cv::viz::WCloud( points, cv::viz::Color::green() );
+        cloud_widget.setRenderingProperty( cv::viz::POINT_SIZE, 4 );
+
+        myWindow.showWidget( "Depth", cloud_widget );
+
+        myWindow.spinOnce(1, true);
+    }
+
+    return 0;
+}
